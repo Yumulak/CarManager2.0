b819a1acbc53ca7404315b2b3777339b616c10b5
Austin Rhodes <92001031+Yumulak@users.noreply.github.com>
2023-09-12 21:26:43 -0400

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CarManager
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+            builder.Services.AddSingleton<CarService>();
+            builder.Services.AddSingleton<CarsViewModel>();
+            builder.Services.AddSingleton<MainPage>();
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ View/DetailsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CarManager.View;
+
+public partial class DetailsPage : ContentPage
+{
+	public DetailsPage()
+	{
+		InitializeComponent();
+	}
+}
